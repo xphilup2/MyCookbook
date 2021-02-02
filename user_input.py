8bfe2d57e6cfd5f584997d067c60e7a651633e2e
Phillip M Hamilton <xphilup@hotmail.com>
2021-02-02 12:15:53 -0800

    Adding my secon hello world program and type casting

---
+++ user_input.py
@@ -0,0 +1,8 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hello " + name)
+
+number = input("What is your favorite number? ")
+# Type casting:
+triple = int(number) * 3
+print("Your number multiplied by 3 is ", triple)
---
+++ variable_type.py
@@ -0,0 +1,7 @@
+my_integer = 3
+my_float = 3.141592
+my_string = "Hello"
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
